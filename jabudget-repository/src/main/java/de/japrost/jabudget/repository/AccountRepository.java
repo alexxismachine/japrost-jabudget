698b79d21e8224aa81e220c57ecd6231789d54fa
alexxismachine <alexxismachine@alexxismachine.de>
2017-06-05 17:23:00 +0200

    start repository

---
+++ jabudget-repository/src/main/java/de/japrost/jabudget/repository/AccountRepository.java
@@ -0,0 +1,53 @@
+package de.japrost.jabudget.repository;
+
+import java.util.Collection;
+import java.util.Optional;
+import java.util.Set;
+
+import de.japrost.jabudget.domain.account.Account;
+
+/**
+ * The repostitory for {@link Account}s,
+ */
+public interface AccountRepository {
+
+	/**
+	 * Create a new {@link Account}.
+	 * 
+	 * @param account the {@link Account} to create.
+	 */
+	// TODO what todo on dublicates?
+	void create(Account account);
+
+	/**
+	 * Update an existing {@link Account}.
+	 * 
+	 * @param account the {@link Account} to update.
+	 */
+	// TODO what todo on not found?
+	void update(Account account);
+
+	/**
+	 * Find an {@link Account} by id.
+	 * 
+	 * @param id the {@link Account} id.
+	 * @return the {@link Account} with the id. An empty {@link Optional} if no {@link Account} is available for the given
+	 *         id.
+	 */
+	Optional<Account> findById(String id);
+
+	/**
+	 * Find all {@link Account}s.
+	 * 
+	 * @return all available {@link Account}s.
+	 */
+	Set<Account> findAll();
+
+	/**
+	 * Delete an {@link Account}.
+	 * 
+	 * @param account the {@link Account} to delete.
+	 */
+	// TODO what todo on not found?
+	void delete(Account account);
+}
---
+++ jabudget-repository/src/main/java/de/japrost/jabudget/repository/inmemory/InMemoryAccountRepository.java
@@ -0,0 +1,41 @@
+package de.japrost.jabudget.repository.inmemory;
+
+import java.util.Optional;
+import java.util.Set;
+
+import de.japrost.jabudget.domain.account.Account;
+import de.japrost.jabudget.repository.AccountRepository;
+
+public class InMemoryAccountRepository implements AccountRepository {
+
+	@Override
+	public void create(Account account) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void update(Account account) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public Optional<Account> findById(String id) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Set<Account> findAll() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void delete(Account account) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
