67f2e5d26e5133d93c0c4966d76524f23c418138
alexxismachine <alexxismachine@alexxismachine.de>
2017-06-11 15:07:32 +0200

    Simple Vaddin UI for listing and creating Accounts

--- jabudget-service/src/main/java/de/japrost/jabudget/service/AccountService.java
+++ jabudget-service/src/main/java/de/japrost/jabudget/service/AccountService.java
@@ -1,5 +1,8 @@
 package de.japrost.jabudget.service;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import de.japrost.jabudget.domain.account.Account;
 import de.japrost.jabudget.repository.AccountRepository;
 
@@ -10,12 +13,20 @@ public class AccountService {
 
 	private final AccountRepository accountRepository;
 
-	public AccountService(AccountRepository accountRepository) {
+	public AccountService(final AccountRepository accountRepository) {
 		this.accountRepository = accountRepository;
 	}
 
+	public Account create(final Account account) {
+		return accountRepository.create(account);
+	}
+
+	public List<Account> retrieveAll() {
+		return new ArrayList<>(accountRepository.findAll());
+	}
+
 	public Account retrieveById(final String id) {
-		accountRepository.create(new Account(id));
+		// TODO throw a business exception on missing account
 		return accountRepository.findById(id).get();
 	}
 }
--- jabudget-vaadin-spring/src/main/java/de/japrost/jabudget/vaadin/spring/JaBudGetUI.java
+++ jabudget-vaadin-spring/src/main/java/de/japrost/jabudget/vaadin/spring/JaBudGetUI.java
@@ -1,16 +1,28 @@
 package de.japrost.jabudget.vaadin.spring;
 
+import com.vaadin.data.HasValue.ValueChangeEvent;
+import com.vaadin.data.provider.ListDataProvider;
 import com.vaadin.server.VaadinRequest;
 import com.vaadin.spring.annotation.SpringUI;
-import com.vaadin.ui.Label;
+import com.vaadin.ui.Button;
+import com.vaadin.ui.FormLayout;
+import com.vaadin.ui.Grid;
+import com.vaadin.ui.HorizontalLayout;
+import com.vaadin.ui.TextField;
 import com.vaadin.ui.UI;
+import com.vaadin.ui.VerticalLayout;
 
+import de.japrost.jabudget.domain.account.Account;
 import de.japrost.jabudget.service.AccountService;
 
 @SpringUI
 public class JaBudGetUI extends UI {
 
 	private final AccountService accountService;
+	private ListDataProvider<Account> accountData;
+	private Button newAccount;
+	private TextField newId;
+	private TextField newName;
 
 	public JaBudGetUI(final AccountService accountService) {
 		this.accountService = accountService;
@@ -18,7 +30,56 @@ public class JaBudGetUI extends UI {
 
 	@Override
 	protected void init(final VaadinRequest request) {
-		setContent(new Label(accountService.retrieveById("Test Account").getId()));
+		final HorizontalLayout mainLayout = new HorizontalLayout();
+		this.setContent(mainLayout);
+		mainLayout.setSizeFull();
+		mainLayout.addComponent(initAccountOverview());
+		mainLayout.addComponent(initNewAccountForm());
 	}
 
+	private VerticalLayout initAccountOverview() {
+		final VerticalLayout layout = new VerticalLayout();
+		final Grid<Account> accounts = new Grid<>(Account.class);
+		accounts.setCaption("Accounts"); // I18N?
+		accounts.setColumns("id", "name");
+		accountData = new ListDataProvider<>(accountService.retrieveAll());
+		accounts.setDataProvider(accountData);
+		layout.addComponent(accounts);
+		return layout;
+	}
+
+	private FormLayout initNewAccountForm() {
+		final FormLayout newAccountLayout = new FormLayout();
+		newId = new TextField();
+		newId.setCaption("id");
+		newId.setRequiredIndicatorVisible(true);
+		newId.addValueChangeListener(this::newIdValueChanged);
+		newAccountLayout.addComponent(newId);
+		newName = new TextField();
+		newName.setCaption("name");
+		newAccountLayout.addComponent(newName);
+		newAccount = new Button();
+		newAccount.setCaption("New");
+		newAccount.setEnabled(false);
+		newAccount.addClickListener(this::createNewAccount);
+		newAccountLayout.addComponent(newAccount);
+		return newAccountLayout;
+	}
+
+	private void createNewAccount(final Button.ClickEvent event) {
+		final Account newAccount = accountService
+				.create(Account.Builder.builder(newId.getValue()).setName(newName.getValue()).build());
+		newId.clear();
+		newName.clear();
+		accountData.getItems().add(newAccount);
+		accountData.refreshAll();
+	}
+
+	private void newIdValueChanged(final ValueChangeEvent<String> event) {
+		if (event.getValue().length() == 0) {
+			newAccount.setEnabled(false);
+		} else {
+			newAccount.setEnabled(true);
+		}
+	}
 }
