3d6af5e64420a4f66b55f208ed032ac453da8c2b
Ulrich David <alexxismachine@alexxismachine.de>
2018-12-29 19:33:59 +0100

    prepare for release
    no SNAPSHOTS
    doc release
    fixes for javadoc

--- jabudget-vaadin-spring/src/main/java/module-info.java
+++ jabudget-vaadin-spring/src/main/java/module-info.java
@@ -2,6 +2,7 @@ module de.japrost.jabudget.vaadin.spring {
     exports de.japrost.jabudget.vaadin.spring;
 
     requires vaadin.server;
+    requires vaadin.shared;
     requires vaadin.spring;
     requires de.japrost.jabudget.domain;
     requires de.japrost.jabudget.service;
